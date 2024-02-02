14cd6d486d750ac952945d1aec1aa087d0761331
David Jeshua Áreas Pérez <142626297+David7J@users.noreply.github.com>
2024-02-01 20:49:43 -0600

    Add files via upload

---
+++ Ejercicios/Class1.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleApp2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Salario s = new Salario();
+            double ig = new double();
+            double eg = new double();
+            double utilidad;
+            int cp;
+
+            do
+            {
+                cp = s.validar("Ingrese la cantidad de personas que desean conocer su utilidad: ");
+
+            } while (cp <= 0 || cp > 10);
+
+            for (int i = 0; i < cp; i++)
+            {
+                do
+                {
+                    ig = s.validar("Digite los ingresos de la persona [" + (i + 1) + "]:");
+                    s.setIngreso(ig);
+                } while (ig < 7000 || ig > 500000);
+
+                do
+                {
+                    eg = s.validar("Digite los egresos de la persona [" + (i + 1 + "]:"));
+                    s.setEgreso(eg);
+                } while (eg < 0 || eg > ig);
+
+
+                utilidad = ig - eg;
+                Console.WriteLine("La utilidad es: " + utilidad);
+            }
+
+
+        }
+    }
+}
---
+++ Ejercicios/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp2
+{
+
+    public class Salario
+    {
+        
+        double ingreso, egreso;
+
+        public Salario() { 
+        
+        }
+
+        public Salario(double ingreso) {
+            this.ingreso = ingreso;
+        }
+
+        public void setIngreso(double ingreso)
+        {
+            this.ingreso = ingreso;
+        }
+
+        public void setEgreso(double egreso)
+        {
+            this.egreso = egreso;
+        }
+
+        public int validar(String Mensaje)
+        {
+            int num = 0, v;
+            do
+            {
+                v = 1;
+                try
+                {
+                    Console.WriteLine(Mensaje);
+                    num = Convert.ToInt32(Console.ReadLine());
+                }catch(Exception error) {
+                    v = 0;
+                    Console.WriteLine("No se aceptan letras ", error.Message);
+                }
+            }while(v == 0);
+            return num;
+        }
+
+
+
+    }
+
+
+}
